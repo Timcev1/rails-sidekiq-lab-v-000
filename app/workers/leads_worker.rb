82b9bac33e1b237be9e29bf5cd603c124cdb85f7
Timothy Cevallos <timcev@gmail.com>
2017-10-04 03:34:58 +0000

    automatically backed up by learn

--- app/workers/leads_worker.rb
+++ app/workers/leads_worker.rb
@@ -1,10 +1,10 @@
 class LeadsWorker
   require 'csv'
   include Sidekiq::Worker
- 
+
   def perform(leads_file)
     CSV.foreach(leads_file, headers: true) do |lead|
-      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+      Customer.create(title: lead[0], Arist.find_or_create_by: lead[1])
     end
   end
 end
